8288f51f498620f541c21dc6a62e407ea921750a
Prakash <rsivasaiprakash121@gmail.com>
2024-05-06 07:47:45 +0530

    added clean_title to main

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import os
 import metadata_title
 
+
+def clean_title(name):
+    if name:
+        invalid_chars = list(r'\:*?"<>|/')
+        for char in invalid_chars:
+            if char in name:
+                name = name.replace(char, ' ')
+    return name
+
+
 pdfs = os.listdir('PDFs')
 
 for pdf in pdfs:
     title = metadata_title.read_title(f'PDFs/{pdf}')
+    title = clean_title(title)
     print(title)
