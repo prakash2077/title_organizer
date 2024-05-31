5f42aca62da11255f9f652a8843ed588c5711f2a
Prakash <rsivasaiprakash121@gmail.com>
2024-05-31 08:01:08 +0530

    Added documentation

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+# for files
 import os
+# extract metadata title
 import metadata_title
 
-
+# filter out invalid characters in the name
 def clean_title(name):
     if name:
         invalid_chars = list(r'\:*?"<>|/')
@@ -10,9 +12,10 @@ def clean_title(name):
                 name = name.replace(char, ' ')
     return name
 
-
+# get all pdfs
 pdfs = os.listdir('PDFs')
 
+# print title for all pdfs
 for pdf in pdfs:
     title = metadata_title.read_title(f'PDFs/{pdf}')
     title = clean_title(title)
--- metadata_title.py
+++ metadata_title.py
@@ -1,6 +1,7 @@
 from PyPDF2 import PdfReader
 
 
+# extract title from metadata
 def read_title(path):
     reader = PdfReader(path)
     title = reader.metadata.title
